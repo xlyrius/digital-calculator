d2dba9d238f9a223be525eac3c6cf0316ee955b2
xlyrius <83902278+xlyrius@users.noreply.github.com>
2025-03-05 06:25:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function appendToDisplay(value) {
-    document.getElementById("display").value += value;
-}
-function clearDisplay() {
-    document.getElementById("display").value = "";
-}
-function calculateResult() {
-    try {
-        document.getElementById("display").value = eval(document.getElementById("display").value);
-    } catch {
-        document.getElementById("display").value = "Error";
-    }
-}
+
